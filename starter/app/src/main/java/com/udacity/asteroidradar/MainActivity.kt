32586144684d7ba259c3d5f3851c709789fc7e8d
Mohamed Atef <proofessergmail.com>
2022-10-08 09:00:58 +0100

    added pushButton and ContentDescription

--- starter/app/src/main/java/com/udacity/asteroidradar/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/MainActivity.kt
@@ -3,15 +3,41 @@ package com.udacity.asteroidradar
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.udacity.asteroidradar.databinding.ActivityMainBinding
 import com.udacity.asteroidradar.main.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
 
     val viewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val nasaApiKey= BuildConfig.NasaApiKey
+
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        setSupportActionBar(binding.toolbar)
+        val navHostFragment=supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController=navHostFragment.navController
+        appBarConfiguration= AppBarConfiguration(
+            setOf(
+                R.id.mainFragment
+            )
+        )
+        setupActionBarWithNavController(navController,
+            appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
     }
 }
