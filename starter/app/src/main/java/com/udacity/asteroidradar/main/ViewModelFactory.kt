da318cabf127ea5f9e90e692d6ccc64392c00574
Mohamed Atef <proofessergmail.com>
2022-10-10 13:36:48 +0100

    changes after gradle upgrade

--- starter/app/src/main/java/com/udacity/asteroidradar/main/ViewModelFactory.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/main/ViewModelFactory.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
 class ViewModelFactory(private val application: Application):ViewModelProvider.Factory {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
             return MainViewModel(application) as T
         }
